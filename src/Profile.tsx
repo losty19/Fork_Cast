535483fb4b8e87537c18067f9cd7977e5f4d0a69
robvarley <rvarley@purdue.edu>
2025-02-18 13:13:01 -0500

    Profile

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,34 +1,24 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
-import { RuxButton, RuxAccordion, RuxAccordionItem, RuxCheckbox } from "@astrouxds/react";
+import { RuxDialog, RuxButton, RuxContainer, RuxInput } from "@astrouxds/react";
 import './Profile.css';
 
 const Profile: React.FC = () => {
-  const navigate = useNavigate();
-
-
+  console.log("Profile page");
+  
   return (
     <div>
-      <RuxAccordion>
-        <RuxAccordionItem>
-          <div slot="label">Vegetables</div>
-          <RuxCheckbox className="checkbox" label="Pepper"></RuxCheckbox>
-          <RuxCheckbox className="checkbox" label="Onion"></RuxCheckbox>
-          <RuxCheckbox className="checkbox" label="Broccoli"></RuxCheckbox>
-          <RuxCheckbox className="checkbox" label="Spinach"></RuxCheckbox>
-        </RuxAccordionItem>
-        <RuxAccordionItem>
-          <div slot="label">Title 2</div>
-          <p>Content 2</p>
-        </RuxAccordionItem>
-        <RuxAccordionItem>
-          <div slot="label">Title 3</div>
-          <p>Content 3</p>
-        </RuxAccordionItem>
-      </RuxAccordion>
+      <RuxContainer className="profile-container">
+        <div slot="header">Profile</div>
+        <div slot="footer">
+          <RuxButton>Save</RuxButton>
+        </div>
+        <div className="profile-content">
+          <RuxInput></RuxInput>          
+        </div>
+      </RuxContainer>
     </div>
-    
   );
 };
 
