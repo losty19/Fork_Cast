0293c85f86a33b3d07135f0f99375dfb3cd2583d
Lucas Ostrander <luke.ostrander19@gmail.com>
2025-03-06 19:10:44 -0500

    Fix save bugs

--- amplify/functions/saveFavoriteRecipe/handler.ts
+++ amplify/functions/saveFavoriteRecipe/handler.ts
@@ -1,11 +1,15 @@
 import type { Schema } from '../../data/resource';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
+const ddbClient = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(ddbClient);
 
 export const handler: Schema["SaveFavoriteRecipe"]["functionHandler"] = async (event) => {
     const { recipeId, userId, title, image } = event.arguments;
 
     // Save the recipe to the database
-    const savedRecipe = await data.models.SavedRecipe.create({
+    const savedRecipe {
         recipeId,
         userId,
         title,
@@ -13,6 +17,19 @@ export const handler: Schema["SaveFavoriteRecipe"]["functionHandler"] = async (e
 
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString()
-    });
-    return savedRecipe;
+    };
+
+    try {
+        await docClient.send(
+            new PutCommand({
+                TableName: "SavedRecipes",
+                Item: savedRecipe
+            })
+        );
+
+        return savedRecipe;
+    } catch (error) {
+        console.error('Error saving recipe:', error);
+        throw new Error('Error saving recipe');
+    }
 }
