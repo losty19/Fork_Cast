108e6fed61f1647b16eb13c441e5e8807fdce354
Lucas Ostrander <luke.ostrander19@gmail.com>
2025-03-10 22:17:26 -0400

    Fix unsued imports/values

--- src/EditRecipe.tsx
+++ src/EditRecipe.tsx
@@ -2,20 +2,20 @@ import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-comp
 import './MainPage.css';
 import './Left_SideBar.css';
 import { useState } from "react";
-import { RuxIcon, RuxDialog } from "@astrouxds/react";
-import { useNavigate } from "react-router-dom";
+import { RuxDialog } from "@astrouxds/react";
+// import { useNavigate } from "react-router-dom";
 
 const EditRecipe = () => {
    const [EditOpen, setEditOpen] = useState(false);
-    const navigate = useNavigate();
-    const [icon, setIcon] = useState("start");
-    const buttonPressed = () => {
-      setEditOpen(!EditOpen);
-    }
+    // const navigate = useNavigate();
+    // const [icon, setIcon] = useState("start");
+    // const buttonPressed = () => {
+    //   setEditOpen(!EditOpen);
+    // }
     return (
             <div>
-	        <RuxDialog open={EditOpen}  header="Header" message="Content goes here" confirm-text="Confirm" deny-text="Cancel">
-	          </RuxDialog>
+	            <RuxDialog open={EditOpen}  header="Header" message="Content goes here" confirm-text="Confirm" deny-text="Cancel">
+	            </RuxDialog>
             </div>
     )
 }
