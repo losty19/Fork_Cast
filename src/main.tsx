d7061ce707357d482c3554b9a9a6eb52a645f551
Lucas Ostrander <luke.ostrander19@gmail.com>
2025-03-10 21:22:56 -0400

    Add AUTH

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from "./HomePage";
 import MainPage from "./MainPage";
-import Profile from "./Profile";
+import Profile from './Profile';
 import RecipeDetails from './RecipeDetails';
 import SearchResultsPage from "./SearchResultsPage";
 import GroceryList from "./GroceryList";
 import "./index.css";
-//import { Amplify } from "aws-amplify";
-//import outputs from "../amplify_outputs.json";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 
-//Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const { user } = useAuthenticator((context) => [context.user]);
@@ -28,49 +28,59 @@ const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Authenticator.Provider>
     <Router>
       <Routes>
         {/* Public Route */}
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<HomePage />} />
 
         {/* Protected Routes */}
         <Route 
           path="/main" 
           element={ 
-            
+            <ProtectedRoute> 
               <MainPage /> 
-            
+            </ProtectedRoute>
           }
         />
         
         <Route 
           path="/profile" 
           element={
+            <ProtectedRoute>
             <Profile />
+            </ProtectedRoute>
           } 
         />
 
         <Route 
           path="/recipeDetails" 
           element={
+            <ProtectedRoute>
             <RecipeDetails />
+            </ProtectedRoute>
           } 
         />
 
         <Route 
           path="/searchResults" 
           element={
+            <ProtectedRoute>
             <SearchResultsPage />
+            </ProtectedRoute>
           } 
         />
 
         <Route 
           path="/grocery-list" 
           element={
+            <ProtectedRoute>
             <GroceryList />
+            </ProtectedRoute>
           } 
         />
 
       </Routes>
     </Router>
+  </Authenticator.Provider>
 );
